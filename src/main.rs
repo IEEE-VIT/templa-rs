b25ccb6f08ddb961b3073f6acd2efbed2b36ef01
Celeo <mattboulanger@fastmail.com>
2021-10-02 14:47:30 -0700

    Fetch submodule info on run instead of compile

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use clap::{load_yaml, App};
+use reqwest::blocking::Client;
+use serde_json::Value;
 
-mod tui;
-mod parse_json;
 mod command;
-mod search;
 mod models;
+mod search;
+mod tui;
+
+const SUBMODULES_URL: &str =
+    "https://api.github.com/repos/IEEE-VIT/templa-rs/contents/submodules.json";
+
+fn fetch_submodules() -> Result<Vec<models::structs::Submodule>, Box<dyn std::error::Error>> {
+    let client = Client::builder()
+        .user_agent("templa-rs, https://github.com/IEEE-VIT/templa-rs")
+        .build()?;
+    let response: Value = client
+        .get(SUBMODULES_URL)
+        .header(
+            reqwest::header::ACCEPT,
+            reqwest::header::HeaderValue::from_static("application/vnd.github.v3+json"),
+        )
+        .send()?
+        .json()?;
+    let encoded_body = response["content"].as_str().unwrap();
+    let decoded_body = base64::decode(encoded_body.replace('\n', "")).unwrap();
+    Ok(serde_json::from_slice(&decoded_body)?)
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let yaml = load_yaml!("../commands.yml");
@@ -18,6 +39,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         proj = String::from(proj_name);
     }
 
-    tui::render_tui(key, &proj);
+    let submodules = match fetch_submodules() {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Could not load submodules from GitHub: {}", e);
+            return Ok(());
+        }
+    };
+
+    tui::render_tui(key, &proj, &submodules);
     Ok(())
 }
--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,10 @@
-use crate::models::structs;
 use crate::models::enums;
+use crate::models::structs;
 
-pub fn perform_search(submodules: Vec<structs::Submodule>, key: String) -> Result<Vec<structs::Submodule>, enums::Error> {
+pub fn perform_search(
+    submodules: &[structs::Submodule],
+    key: String,
+) -> Result<Vec<structs::Submodule>, enums::Error> {
     let mut filtered_sm = vec![];
     for submodule in submodules.iter() {
         if submodule.tags.contains(&key) {
