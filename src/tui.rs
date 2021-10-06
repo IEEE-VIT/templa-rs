0f461ff1091a3b14fa7767a435859c7c9195a18e
Daemond <daemondeal@gmail.com>
2021-10-06 10:46:30 +0200

    feat: added live search bar

--- src/tui.rs
+++ src/tui.rs
@@ -2,7 +2,7 @@ use crate::command;
 use crate::models::{enums, structs::Submodule};
 use crate::app::App;
 use crossterm::{
-    event::{self, Event as CEvent, KeyCode},
+    event::{self, Event as CEvent, KeyCode, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
@@ -15,18 +15,30 @@ use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
-    text::{Span, Spans},
-    widgets::{Block, BorderType, Borders, List, ListItem, ListState},
+    text::{Span, Spans, Text},
+    widgets::{Block, BorderType, Borders, List, ListItem, ListState, Paragraph},
     Terminal,
 };
 
-pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
+pub fn render_tui(proj_name: &str, submodules: Vec<Submodule>, optional_first_query: Option<&str>, optional_tag: Option<&str>,) {
     enable_raw_mode().expect("can run in raw mode");
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen).unwrap();
 
     // App state container
-    let mut app = App::new(submodules);
+    let mut full_first_query = String::new();
+    
+    if let Some(tag_raw) = optional_tag {
+        full_first_query.push_str("tag:");
+        full_first_query.push_str(tag_raw);
+        full_first_query.push(' ');
+    }
+    
+    if let Some(first_query) = optional_first_query {
+        full_first_query.push_str(first_query);
+    }
+
+    let mut app = App::new(submodules, &full_first_query);
 
     let (tx, rx) = mpsc::channel();
     let tick_rate = Duration::from_millis(200);
@@ -43,10 +55,8 @@ pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
                 }
             }
 
-            if last_tick.elapsed() >= tick_rate {
-                if let Ok(_) = tx.send(enums::Event::Tick) {
-                    last_tick = Instant::now();
-                }
+            if last_tick.elapsed() >= tick_rate && tx.send(enums::Event::Tick).is_ok() {
+    	        last_tick = Instant::now();
             }
         }
     });
@@ -58,7 +68,7 @@ pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
     template_list_state.select(Some(0));
 
     loop {
-        app.search(&key);
+        app.search();
 
         terminal
             .draw(|rect| {
@@ -66,23 +76,43 @@ pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
                     .margin(2)
-                    .constraints([Constraint::Percentage(80)].as_ref())
+                    .constraints(
+                        [
+                            Constraint::Percentage(15),
+                            Constraint::Percentage(80),
+                        ].as_ref()
+                    )
                     .split(size);
 
-                let template_list = render_template_list(&app.filtered_submodules);
-                rect.render_stateful_widget(template_list, chunks[0], &mut template_list_state);
+                let template_list = render_template_list(&app.filtered_submodules, app.skin_color);
+                let template_search = render_search_bar(&app.current_query, app.skin_color);
+
+                rect.render_widget(template_search, chunks[0]);
+                rect.render_stateful_widget(template_list, chunks[1], &mut template_list_state);
             })
             .unwrap();
 
         match rx.recv().unwrap() {
-            enums::Event::Input(event) => match event.code {
-                KeyCode::Char('q') => {
+            enums::Event::Input(event) => match (event.code, event.modifiers) {
+                (KeyCode::Char(key), KeyModifiers::NONE) => {
+                    app.current_query.push(key);
+                }
+                (KeyCode::Char(key), KeyModifiers::SHIFT) => {
+                    app.current_query.push(key.to_ascii_uppercase());
+                }
+                (KeyCode::Backspace, KeyModifiers::NONE) => {
+                    app.current_query.pop();
+                }
+                (KeyCode::Backspace, KeyModifiers::CONTROL) => {
+                    app.current_query.clear();
+                }
+                (KeyCode::Char('c'), KeyModifiers::CONTROL) => {
                     disable_raw_mode().unwrap();
                     execute!(terminal.backend_mut(), LeaveAlternateScreen,).unwrap();
                     terminal.show_cursor().unwrap();
                     break;
                 }
-                KeyCode::Down => {
+                (KeyCode::Down, _) => {
                     if let Some(selected) = template_list_state.selected() {
                         let templates_length = app.filtered_submodules.len();
                         if selected >= templates_length - 1 {
@@ -92,7 +122,7 @@ pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
                         }
                     }
                 }
-                KeyCode::Up => {
+                (KeyCode::Up, _) => {
                     if let Some(selected) = template_list_state.selected() {
                         let templates_length = app.filtered_submodules.len();
                         if selected > 0 {
@@ -102,8 +132,8 @@ pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
                         }
                     }
                 }
-                KeyCode::Enter => {
-                    if let Some(_) = template_list_state.selected() {
+                (KeyCode::Enter, _) => {
+                    if template_list_state.selected().is_some() {
                         let templates = &app.filtered_submodules;
                         let selected_template = templates
                             .get(
@@ -137,11 +167,28 @@ pub fn render_tui(key: String, proj_name: &str, submodules: Vec<Submodule>) {
     }
 }
 
-fn render_template_list<'a>(filtered_submodules: &[Submodule]) -> List<'a> {
+fn render_search_bar(search_query: &str, skin_color: Color) -> Paragraph {
+    let mut search_content = String::new();
+    search_content.push_str(" / ");
+    search_content.push_str(search_query);
+
+    Paragraph::new(Text::from(search_content)).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .style(Style::default().fg(skin_color))
+    ).style(Style::default().fg(Color::White))
+}
+
+fn render_template_list<'a>(filtered_submodules: &[Submodule], skin_color: Color) -> List<'a> {
+    let title_span = Span::styled(
+        " Available Templates ",
+        Style::default().fg(skin_color).add_modifier(Modifier::BOLD)
+    );
+
     let pets = Block::default()
         .borders(Borders::ALL)
-        .style(Style::default().fg(Color::White))
-        .title("Available Templates")
+        .style(Style::default().fg(skin_color))
+        .title(title_span)
         .border_type(BorderType::Plain);
 
     let items: Vec<_> = filtered_submodules
@@ -149,7 +196,7 @@ fn render_template_list<'a>(filtered_submodules: &[Submodule]) -> List<'a> {
         .map(|submodule| {
             ListItem::new(Spans::from(vec![Span::styled(
                 submodule.name.clone(),
-                Style::default(),
+                Style::default().fg(Color::White),
             )]))
         })
         .collect();
