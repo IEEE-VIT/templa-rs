2b1cb062cc73681b4f38facffb7e5ef7cdc6c44c
Michael D <m.dormand@sky.com>
2021-10-10 00:32:20 +0100

    Change to a different, faster linear search

--- src/search.rs
+++ src/search.rs
@@ -1,5 +1,8 @@
 use crate::models::enums;
 use crate::models::structs;
+use crate::models::structs::Submodule;
+use std::time::{Duration, Instant};
+use std::thread::sleep;
 
 pub fn perform_search(
     submodules: &[structs::Submodule],
@@ -9,11 +12,18 @@ pub fn perform_search(
     let mut filtered_sm = vec![];
     let lowercase_query = name_query.to_lowercase();
 
-    for submodule in submodules.iter() {
-        if (tags.is_empty() || submodule.has_one_of_tags(tags)) && 
-            (name_query.is_empty() || submodule.name.to_lowercase().contains(&lowercase_query)) {
-            filtered_sm.push(submodule.clone());
-        }
+    let x = submodules.len();
+
+    let mut i = 0;
+
+    while i < x {
+        if (tags.is_empty() || submodules[i].has_one_of_tags(tags)) && 
+            (name_query.is_empty() || submodules[i].name.to_lowercase().contains(&lowercase_query)) {
+                filtered_sm.push(submodules[i].clone());
+        } 
+
+        i+=1;
     }
+
     Ok(filtered_sm)
 }
